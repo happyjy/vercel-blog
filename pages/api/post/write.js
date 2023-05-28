5501c4d2e044190ae50953c6b75b5667b1b4fd7b
happyjy <okwoyjy@gmail.com>
2023-05-28 14:40:51 +0900

    fix: write시 throw error 제거

--- pages/api/post/write.js
+++ pages/api/post/write.js
@@ -5,7 +5,6 @@ export default async function handler(req, res) {
   const { id, title, content } = req.body;
 
   try {
-    throw new Error('errrrrrrrror');
     await createPost({
       id,
       title,
